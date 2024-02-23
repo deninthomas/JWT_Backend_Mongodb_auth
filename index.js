a41ec51f2e9725a67f40753e3b97bdc5896e5184
deninthomas <deninthomas@outlook.com>
2024-02-23 12:55:23 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connectDB = require('./db');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+require('dotenv').config({path:'./touch.env'}); // Load environment variables
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Check if MONGODB_URI is defined
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI environment variable is not defined.");
+  process.exit(1); // Exit the application
+}
+
+// Connect to MongoDB
+connectDB();
+
+// Parse JSON request body
+app.use(express.json());
+
+// Define authentication routes
+app.use('/auth', authRoutes);
+
+// Define user routes
+app.use('/user', userRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
